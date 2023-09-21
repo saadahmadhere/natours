f92df25ee4f1b78528a1fb39cc5f1aebe3269042
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-21 13:49:13 +0530

    [chore]: serving static html from the code

--- app.js
+++ app.js
@@ -8,23 +8,21 @@ const app = express();
 
 // MIDDLEWARES
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`)); // this middleware sets this as route when any of the defined routes cannot be found. So, if we haven't defined any routes /public, this middleware will put it as the default route.
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 🙋‍♀️')
+  console.log('Hello from the middleware 🙋‍♀️');
   next();
-})
+});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 //mounting the routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-
 module.exports = app;
-
