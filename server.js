184645d4370403354c03d5d72c14bcea297cc021
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-26 16:07:03 +0530

    check process.env variables

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const app = require('./app')
+const app = require('./app');
+
+// console.log(app.get('env'));
+process.env.NODE_ENV = 'development';
+console.log(process.env);
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
-  console.log('💥Server running on port ' + PORT + "💥");
+  console.log('💥Server running on port ' + PORT + '💥');
 });
