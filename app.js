48bf5413e4926ccf4eb7952e123eab4b741dc8e0
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-27 06:18:35 +0530

    configure dotenv and add prod scripts

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const app = express();
+const app = express()
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV)
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev')); // this is to get the logger (like GET /api/v1/tours 200 6.176 ms - 8817)
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`)); // this middleware sets this as route when any of the defined routes cannot be found. So, if we haven't defined any routes /public, this middleware will put it as the default route.
 
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'}); // setting the dotenv config before requiring app.
+
 const app = require('./app');
 
 // console.log(app.get('env'));
-process.env.NODE_ENV = 'development';
-console.log(process.env);
+// process.env.NODE_ENV = 'development';
+// console.log(process.env.NODE_ENV);
 
-const PORT = 8000;
+const PORT = process.env.port || 8000;
 
 app.listen(PORT, () => {
   console.log('💥Server running on port ' + PORT + '💥');
