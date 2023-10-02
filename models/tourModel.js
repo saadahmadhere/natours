e278de4a44dcf60888e012ab85903f82b0224b4f
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-10-02 17:22:59 +0530

    refactor tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -11,6 +11,34 @@ const toursSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  discount: Number,
+  duration: { type: Number, required: [true, 'A tour must have a duration'] },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: { type: Number, default: 4.0 },
+  ratingsQuantity: { type: Number, default: 4.0 },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: { type: String, trim: true },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
