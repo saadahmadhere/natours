2cd30ddd782a068b52a0dfc67e955e04ca07d012
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-11-28 07:24:59 +0530

    118. errors in development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,41 @@
+const sendDevelopmentError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendProductionError = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or other unknown error: don't leak error details.
+
+    // 1) Log the error.
+    console.error('Error: 💥', err);
+
+    // 2) send generic message.
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendDevelopmentError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProductionError(err, res);
+  }
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
